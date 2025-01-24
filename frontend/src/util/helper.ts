7039587f53405d811ec43a5a2a56d038b1ad8b25
Aiden Lee <aidentlee11@gmail.com>
2025-01-24 07:03:12 -0600

    update: have folders open at 7am instead of 8am

--- frontend/src/util/helper.ts
+++ frontend/src/util/helper.ts
@@ -1,14 +1,14 @@
 import { LocationOperatingTimes, OperationHoursData, OperatingTime } from "../types/OperationTypes";
 
 // Returns
-// -- Breakfast if current time is between 8:00 AM and 10:59 AM
+// -- Breakfast if current time is between 7:00 AM and 10:59 AM
 // -- Lunch if current time is between 11:00 AM and 4:59 PM
 // -- Dinner if current time is between 5:00 PM and 7:59 PM
 // -- Empty string if current time is outside of the above ranges
 export const getCurrentTimeOfDay = (): string => {
   const currentTime = new Date();
   const currentHour = currentTime.getHours();
-  if (currentHour >= 8 && currentHour <= 10) {
+  if (currentHour >= 7 && currentHour <= 10) {
     return "Breakfast";
   } else if (currentHour >= 11 && currentHour <= 16) {
     return "Lunch";
